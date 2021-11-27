2ae518b7e4706c0a85fe20d9d70937aa1ec0e9ef
leon-yarov <leon44ml@gmail.com>
2021-11-27 21:04:03 +0200

    moved logic to jQuery

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,54 +1,55 @@
-let draggables = [];
+var $draggable = $('.draggable').draggabilly({
+    containment: true,
+});
+
+let connection = {
+    "from": 0,
+    "to": 0
+}
+//TODO: figure out how to connect gates
+const connections = [];
+
 //get all dragable elements on HTML page on startup
 
 //draggable blocks class
-class Draggable {
-    constructor(el) {
-        //draggable element reference
-        this.draggableEl = el;
-        //init API for draggable element
-        this.draggie = new Draggabilly(el,
-            {
-                containment: true,
-                // grid: [30, 30],
-            });
-        //starting position
-        this.originPos = Object.assign({}, this.draggie.position);
-    }
-}
-//init draggable blocks on startup
 
-
-const setSizes = (elements) => {
-    elements.forEach((item, i) => {
-        item.style.setProperty('--width', `50px`);
-        item.style.setProperty('--border-radius', `5px`);
+const setSizes = () => {
+    $('.draggable').each(function () {
+        $(this).css('--width', `50px`);
+        $(this).css('--border-radius', `5px`);
     });
 };
 
 const init_gates = () => {
-    let draggableBlocks = document.querySelectorAll('.block.draggable');
-    draggables = Array.from(draggableBlocks).map(block => new Draggable(block));
-    setSizes(draggableBlocks);
-    draggables.forEach(draggable => draggable.draggie.on('dragMove', listener));
+    $draggable = $('.draggable').draggabilly({
+        containment: true,
+    });
+    setSizes();
+    $draggable.on('dragMove', listener);
 }
 
 const addBlock = () => {
-    document.getElementById("blocks").innerHTML += new_gate;
+    //gate text defined here
+    document.getElementById("blocks").innerHTML += new_gate('AND');
+    //init draggable gates
     init_gates();
 };
 
 
 function listener() {
-    $('#l1').attr('x1', getPos(last_input)[0] - $('svg').position().left);
-    $('#l1').attr('y1', getPos(last_input)[1] - $('svg').position().top);
-    $('#l1').attr('x2', getPos(last_output)[0] - $('svg').position().left);
-    $('#l1').attr('y2', getPos(last_output)[1] - $('svg').position().top);
+    var draggie = $(this).data('draggabilly');
+    $('.wire').each(function () {
+        $(this).attr('x1', getPos(last_input)[0] - $('svg').position().left);
+        $(this).attr('y1', getPos(last_input)[1] - $('svg').position().top);
+        $(this).attr('x2', getPos(last_output)[0] - $('svg').position().left);
+        $(this).attr('y2', getPos(last_output)[1] - $('svg').position().top);
+    });
 }
-document.addEventListener("keydown", addBlock); const main = () => {
+document.addEventListener("keydown", addBlock);
+
+const main = () => {
     init_gates();
 };
 
 main();
 
-document.addEventListener("keydown", addBlock);
