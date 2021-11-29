5eef09dffd0cfc1b9174364951117b84c9105d23
leon-yarov <leon44ml@gmail.com>
2021-11-29 17:40:32 +0200

    j

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
+
 app = Flask(__name__, static_url_path="")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -56,7 +58,7 @@ def signin():
         if user.user_type == 'student':
             return render_template('/dashboard3.html')
         if user.user_type == 'teacher':
-            return render_template('/dashboard2.html')
+            return render_template('/dashboard2.html', rooms=Room.query.all())
         if user.user_type == 'director':
             return render_template('/dashboard1.html', users_db=User.query.all())
     else:
@@ -76,6 +78,14 @@ def erase(id):
     return redirect('/dashboard3.html')
 
 
+@app.route('/deleter/<int:id>')
+def eraser(id):
+    data = Room.query.get(id)
+    db.session.delete(data)
+    db.session.commit()
+    return redirect('/dashboard2.html')
+
+
 @app.route('/Login/', methods=['GET', 'POST'])
 def login():
     return render_template('Login.html')
@@ -86,7 +96,36 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/update/<int:id>', methods=['GET','POST'])
+class Room(db.Model):
+    __tablename__ = 'rooms'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(256))
+    name = db.Column(db.String(80))
+    users = db.Column(db.String(256))
+
+    def __repr__(self):
+        return '<Room %r>' % (self.name)
+
+
+@app.route('/newroom', methods=['POST'])
+def newroom():
+    description = request.form['description']
+    type = request.form['name']
+    room = Room(name=type, description=description, users='')
+    db.session.add(room)
+    db.session.commit()
+    return 'new room created'
+
+
+@app.route('/assign/<int:rid>/<int:id>')
+def assign(rid, id):
+    room = Room.query.get_or_404(rid)
+    student = User.query.get_or_404(id)
+    room.users += ', ' + student.name
+    return 'assigned'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     data = User.query.get(id)
     if request.method == "POST":
@@ -99,6 +138,7 @@ def update(id):
     else:
         return render_template('Bad Username')
     return redirect('/dashboard3.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
