93dcef9c6dbb3ca92c8721b65833561b19558730
leon-yarov <leon44ml@gmail.com>
2021-12-16 23:21:11 +0200

    Added backend - Node.js

---
+++ LogiGate/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views')); //set the views directory
+app.set('view engine', 'hbs'); //set the view engine
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 ## LogiGate
-# https://dori654.github.io/LogiGate/
+
+
+## What happened
+1. The 'LogiGate' Folder is node.js folder
+2. Conversion to node.js began my friends
+
+
+## How to Run:
+- Install Node.Js from their website
+- On first launch use ```node install```
+- Make sure nodemon is installed on your computer: ```npm install nodemon --g```
+- Navigate to the project folder ```cd LogiGate```
+- On every launch use ```npm run dev```
+
+
+
