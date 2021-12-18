98e88eb10869a9c440298953c6fdfc065191e0e1
leon-yarov <leon44ml@gmail.com>
2021-12-18 23:26:00 +0200

    dashboard route

--- LogiGate/app.js
+++ LogiGate/app.js
@@ -3,8 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Require route paths
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dashboardRouter = require('./routes/dashboard');
 
 var app = express();
 
@@ -17,7 +19,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views')); //set the views directory
 app.set('view engine', 'hbs'); //set the view engine
 
+//Tell node to use this links as paths
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dashboard', dashboardRouter);
 
 module.exports = app;
