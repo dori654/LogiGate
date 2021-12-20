72f15947bd71f601681b1ccc130b00296b3efe4f
Dori Fourer <dori.fourer654@gmail.com>
2021-12-20 18:32:40 +0200

    Database - MongoDB

--- LogiGate/routes/dashboard.js
+++ LogiGate/routes/dashboard.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
 
-router.get('/dashboard', function (req, res) {
-    res.render('dashboard', {
-        title: 'Dashboard',
-        layout: 'dashboard_layout'
-    });
-})
+router.get("/dashboard", function (req, res) {
+  res.render("dashboard", {
+    title: "Dashboard",
+    layout: "dashboard_layout",
+  });
+});
 
 module.exports = router;
