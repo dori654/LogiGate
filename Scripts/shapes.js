27cb5970d12e377f8dce05eb1a52f10ae84c48fb
Dori Fourer <dori.fourer654@gmail.com>
2021-12-21 21:56:56 +0200

    f

--- Scripts/shapes.js
+++
@@ -1,7 +0,0 @@
-
-
-const new_gate = (type) => {
-    return '<div class="block draggable" data - id="' + draggables.length + '" ><div class="input"></div><span class="gate">' + type + '</span><div class="input"></div><div class="output"></div></div>'
-};
-
-
