49c4533674fa069ead53dbc4c6a3a529060b5e68
leon-yarov <leon44ml@gmail.com>
2021-12-23 19:19:45 +0200

    Users list

--- LogiGate/routes/dashboard.js
+++ LogiGate/routes/dashboard.js
@@ -1,11 +1,26 @@
 var express = require("express");
 var router = express.Router();
+var userDB = require("../models/user");
 
 router.get("/dashboard", function (req, res) {
-  res.render("dashboard", {
-    title: "Dashboard",
-    layout: "dashboard_layout",
+
+  userDB.find({}, function (err, foundUser) {
+    res.render("dashboard", {
+      title: "Dashboard",
+      layout: "dashboard_layout",
+      students: foundUser
+    });
   });
 });
 
+router.get("/remove/:_id", function (req, res) {
+  userDB.findById(req.params._id, function (err, foundUser) {
+    userDB.deleteOne(foundUser, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+    res.redirect(req.get('referer'));
+  });
+});
 module.exports = router;
