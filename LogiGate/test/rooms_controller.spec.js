d11f26d91460b47b4de1602b90db118a155a0e6b
Dori Fourer <dori.fourer654@gmail.com>
2021-12-30 19:15:57 +0200

    rooms controller testing

--- LogiGate/test/rooms_controller.spec.js
+++ LogiGate/test/rooms_controller.spec.js
@@ -5,3 +5,33 @@ const request = require('supertest');
 const express = require('express');
 const app = express();
 
+
+describe('GET\' /rooms/create', function() {
+    it('responds with html', function(done) {
+        request(app)
+            .get('/rooms/create')
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /html/)
+            .expect(404, done);
+    });
+});
+
+describe('GET\' /rooms/edit/:_id', function() {
+    it('responds with html', function(done) {
+        request(app)
+            .get('/rooms/edit/:_id')
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /html/)
+            .expect(404, done);
+    });
+});
+
+describe('GET\' /rooms/remove/:_id', function() {
+    it('responds with html', function(done) {
+        request(app)
+            .get('/rooms/remove/:_id')
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /html/)
+            .expect(404, done);
+    });
+});
