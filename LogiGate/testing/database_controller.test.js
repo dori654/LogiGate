d1d7c5b8833b8a63ffe2c2de4438b17166d48dba
Dori Fourer <dori.fourer654@gmail.com>
2021-12-30 12:59:35 +0200

    .

--- LogiGate/testing/database_controller.test.js
+++
@@ -1,8 +0,0 @@
-// const { CheckLogin,} = require('/controller/database_controller.js');
-
-const request = require('supertest');
-const mongoose = require("mongoose");
-const { MongoMemoryServer } = require("mongodb-memory-server");
-
-const database_controller = require('../controller/database_controller');
-const user = require('../models/user')
