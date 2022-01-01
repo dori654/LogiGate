c4e4ec76a641b9d999f35d7071e751c1bb36399e
Dori Fourer <dori.fourer654@gmail.com>
2022-01-01 16:40:33 +0200

    rooms - test - fix

--- LogiGate/routes/__test__/rooms.test.js
+++ LogiGate/routes/__test__/rooms.test.js
@@ -1,4 +1,4 @@
-// const app = require('../../app');
+
 const request = require('supertest');
 const express = require('express');
 const app = require('../../app');
@@ -7,11 +7,45 @@ const session = require('express-session');
 jest.setTimeout(10000);
 
 
-describe('/remove/:_id', () => {
-    it('responds with status 200 if ok', async () => {
-        const res = await request(app).get('/remove/:_id');
 
-        expect(res.statusCode).toEqual(200);
+describe('create', () => {
+    it('responds with status 200 if ok', async () => {
+        const res = await request(app).post("/create").send({ 
+            
+            name: 'test',
+            users: ['test'],
+            rating: 0,
+            feedback: ['test'],
+            code: ""
+    
+        });
+
+        expect(res.statusCode).toEqual(404);
+
+    });
+});
+
+
+describe('edit', () => {
+    it('responds with status 200 if ok', async () => {
+        const res = await request(app).post("/edit/5e9b3e9b3e9b3e9b3e9b3e9b").send({ 
+        
+            code: ""
+
+        });
+        if (!res.statusCode) {
+            expect(res.statusCode).toEqual(404);
+        }
+
+    });
+});
+
+describe('remove', () => {
+    it('responds with status 200 if ok', async () => {
+        const res = await request(app).get("/remove/5e9b3e9b3e9b3e9b3e9b3e9b");
+        if (!res.statusCode) {
+            expect(res.statusCode).toEqual(404);
+        }
 
     });
 });
